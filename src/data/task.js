748b1ae5ee873d9f3469738693dae81025da1505
CanarioEchazu <darioechazu@hotmail.com>
2023-01-06 12:46:28 -0300

    first commit

---
+++ src/data/task.js
@@ -0,0 +1,23 @@
+export const tasks = [
+    {
+      id: 0,
+      title: "tarea 1",
+      description: "Despertar, agradecerle a Dios y ducharme",
+    },
+    {
+      id: 1,
+      title: "tarea 2",
+      description: "Cambiarme para aprovechar el nuevo día",
+    },
+    {
+      id: 2,
+      title: "tarea 3",
+      description: "Hacer la cama, bien  tendida",
+    },
+    {
+      id: 3,
+      title: "tarea 4",
+      description: "Desayunar bien variado y feliz",
+    },
+  ];
+  
